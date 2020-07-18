bfa00dbabc788155b2c302cc3111fe77e12d481b
Manoj Majumdar <manojmajumdarb92@gmail.com>
2020-07-18 20:29:25 +0530

    First Commit

---
+++ src/main/java/jenkins/java/sample/App.java
@@ -0,0 +1,13 @@
+package jenkins.java.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
